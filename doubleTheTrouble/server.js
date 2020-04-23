a4bccda6e90b60dcca8ca9618e4bb0ec770a0bbe
Felipe Garcia Diaz <41710468+felipeGarciaDiaz@users.noreply.github.com>
2020-04-23 04:08:11 -0400

    updated server.js
    Created DB, Created name handler, and highscore board updater

--- doubleTheTrouble/server.js
+++ doubleTheTrouble/server.js
@@ -1,61 +1,94 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 
-var PORT = 45050;
+var PORT = 3332;
 
 app.use("/", express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/gamedata", {
-    useNewUrlParser: true
+mongoose.connect("mongodb://localhost/playerDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+var db = mongoose.connection;
+db.once('open', function(cb) {
+    console.log("connection established");
+
 });
 
 
-var gameSchema = new mongoose.Schema({
-    nickname: String
-});
-var User = mongoose.model("User", gameSchema);
 
+io.on('connection', function(socket) {
+    console.log('user connected ' + socket.id + " ip " + socket.conn.remoteAddress);
 
+    socket.on("chooseName", function(newName) {
 
+        var data = {
+            nickname: newName.toUpperCase(),
+            highscore: 0
+        };
+        db.collection('playerStats').findOne({nickname: data.nickname }, function(err, doc) {
+            if(err) throw err;
 
+            if(doc) {
+                io.to(socket.id).emit('nnError', 1);
+            } else if( !newName.match(/^[a-z0-9]+$/i)) {
+                io.to(socket.id).emit('nnError', 2);
+            } else if (newName.length > 15) {
+                io.to(socket.id).emit('nnError', 3);
+            }else{
+                db.collection('playerStats').insertOne(data, function(err) {
+                    console.log(doc + "  |}}}}  " + JSON.stringify(data.nickname));
 
+                    if(err) throw err;
+                    console.log('rec estab');
+                    io.to(socket.id).emit("newNickname", null);
+                });
+            }
 
+        });
+    });
 
+    socket.on('player', function (player) {
 
+        socket.on('highscore', function (hs) {
+            console.log(player.toUpperCase() + ": " + hs);
+            db.collection('playerStats').findOne({nickname: player.toUpperCase()}, function (err) {
+                console.log('found player');
 
+                if(err) throw err;
+                db.collection('playerStats').updateOne({nickname: player.toUpperCase()}, {$set: {highscore: hs}});
+                console.log('highscore updated');
+            });
+        });
 
+    });
 
-
-
-
-app.post("/addNickname", (req, res) =>{
-    var playerNickname = new User(req.body.pickName);
-    playerNickname.save()
-    .then(item => {
-        console.log("nickname created")
-        res.send("item saved to database");
-
-    })
-    .catch(err => {
-        res.status(400).send("unable to save to database");
+    var dbOverflow = false;
+    socket.on('hsGo', function() {
+        if(dbOverflow === false) {
+            db.collection('playerStats')
+                .find({}, {projection: {_id: 0}})
+                .sort({highscore : -1})
+                .limit(100).toArray()
+                .then(function (doc) {
+                    io.to(socket.id).emit('topPlayers', doc);
+                    dbOverflow = true;
+                });
+        }
     });
 });
 
 
 
 
-
-
-
-
-
-app.listen(PORT, function () {
-    console.log("server is up and running using port " + PORT)
+http.listen(PORT, function () {
+    console.log("server is up and running using port " + PORT);
 });
